bc32237b3b6657b0a1a7b7b0aaa3850092d3d8ef
Elsin617 <elsonjunior831@gmail.com>
2025-03-31 04:12:01 +0000

    Acentuei algumas palavras no printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,22 +14,22 @@ int main() {
     printf("Digite o estado 01:\n");
     scanf(" %c", &estado_01); 
 
-    printf("Digite o codigo da carta 01:\n");
+    printf("Digite o código da carta 01:\n");
     scanf("%s", codigo_carta_01);
 
     printf("Digite a cidade 01:\n");
     scanf(" %[^\n]", cidade_01);  
 
-    printf("Digite a populacao 01:\n");
+    printf("Digite a população 01:\n");
     scanf("%d", &populacao_01);
 
-    printf("Digite a area 01:\n");
+    printf("Digite a área 01:\n");
     scanf("%f", &area_01);
 
     printf("Digite o PIB 01:\n");
     scanf("%f", &PIB_01);
 
-    printf("Digite o numero de pontos turisticos 01:\n");
+    printf("Digite o número de pontos turísticos 01:\n");
     scanf("%d", &num_pontos_tur_01);
 
     printf("**** ENTRADA DE DADOS CARTA O2 ****\n");
@@ -37,22 +37,22 @@ int main() {
     printf("Digite o estado 02:\n");
     scanf(" %c", &estado_02); 
 
-    printf("Digite o codigo da carta 02:\n");
+    printf("Digite o código da carta 02:\n");
     scanf("%s", codigo_carta_02);
 
     printf("Digite a cidade 02:\n");
     scanf(" %[^\n]", cidade_02); 
 
-    printf("Digite a populacao 02:\n");
+    printf("Digite a população 02:\n");
     scanf("%d", &populacao_02);
 
-    printf("Digite a area 02:\n");
+    printf("Digite a área 02:\n");
     scanf("%f", &area_02);
 
     printf("Digite o PIB 02:\n");
     scanf("%f", &PIB_02);
 
-    printf("Digite o numero de pontos turisticos 02:\n");
+    printf("Digite o número de pontos turísticos 02:\n");
     scanf("%d", &num_pontos_tur_02);
 
     printf("\n=== Dados informados ===\n");
